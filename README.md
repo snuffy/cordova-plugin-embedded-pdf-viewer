b920a5bd76b51f74e4a72c6e9d512a145ea1c45a
Florian Pechwitz <florian.pechwitz@itizzimo.com>
2017-05-19 14:59:23 +0200

    - Added Windows platform - Changed namespace to PdfViewer - Updated README

--- README.md
+++ README.md
@@ -15,15 +15,15 @@ $ cordova plugin add https://stash.itizzimo.com/scm/cor/cordova-plugin-native-pd
 
 ```
 var options = { 
-                headerColor:"#000000", //Not supported under iOS
-                tintColor: "#000000", //Not supported under Android
-                showScroll:true, //Not supported under iOS
-                showShareButton:true, //Not supported under iOS
-                showCloseButton:true, //Not supported under iOS
-                swipeHorizontal:false //Not supported under iOS
+                headerColor:"#000000",  //Not supported under iOS and Windows
+                tintColor: "#000000",   //Not supported under Android and Windows
+                showScroll:true,        //Not supported under iOS and Windows
+                showShareButton:true,   //Not supported under iOS and Windows
+                showCloseButton:true,   //Not supported under iOS and Windows
+                swipeHorizontal:false   //Not supported under iOS and Windows
               };
               
-AndroidNativePdfViewer.openPdfUrl(uri, title, options, 
+PdfViewer.openPdfUrl(uri, title, options, 
                         function(success){
                         // success callback
                         },function(error){
@@ -34,3 +34,15 @@ AndroidNativePdfViewer.openPdfUrl(uri, title, options,
 ### Sources
 https://github.com/neinhart/cordova-plugin-android-native-pdfviewer  
 https://github.com/shlomnissan/cordova-plugin-simple-pdf  
+https://github.com/sitewaerts/cordova-plugin-document-viewer  
+
+### License
+The MIT License
+
+Copyright (c) 2008-2016 Betamark
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ www/PdfViewer.js
@@ -0,0 +1,131 @@
+/*module.exports = {
+ openPdf: function (url, header, options, success, error) {
+ switch(device.platform){
+ case "Android":
+ exec(success, error, "AndroidNativePdfViewer", "openPdfUri", [uri, title, options]);
+ break;
+ case "iOS":
+ exec(success, error, "SimplePDF", "showPDF", [ path, title, options.tintColor ]);
+ break;
+ default:
+ error("Your platform is not supported.");
+ }
+ }
+ };*/
+
+var exec = require('cordova/exec');
+
+exports.openPdf = function (uri, title, options, success, error) {
+    switch (device.platform) {
+        case "Android":
+            exec(success, error, "AndroidNativePdfViewer", "openPdfUri", [uri, title, options]);
+            break;
+        case "iOS":
+            exec(success, error, "SimplePDF", "showPDF", [uri, title, options.tintColor]);
+            break;
+        case "windows":
+            windowsPdf(uri, function () {}, success, error, error);
+            break;
+        default:
+            error("Your platform is not supported.");
+    }
+};
+
+function windowsPdf(url, onShow, onClose, onMissingApp, onError) {
+    var temp = document.getElementsByTagName('base')[0].href;
+    document.getElementsByTagName('base')[0].href = "";
+    var contentType = "application/pdf";
+    var JS_HANDLE = "SitewaertsDocumentViewer";
+    var CDV_HANDLE = "SitewaertsDocumentViewer";
+    var CDV_HANDLE_ACTIONS = {
+        VIEW_DOCUMENT: "viewDocument"
+    };
+
+    var errorPrefix = "Error in " + JS_HANDLE + ".viewDocument(): ";
+
+    var _hideStatusBarOnClose = false;
+
+    function _beforeShow(next) {
+        if (next)
+            next();
+    }
+
+    function _onShow() {
+        if (onShow)
+            onShow();
+    }
+
+    function _beforeClose(next) {
+        if (_hideStatusBarOnClose) {
+            _hideStatusBarOnClose = false;
+            window.StatusBar.hide();
+        }
+
+        document.getElementsByTagName('base')[0].href = temp;
+        if (next)
+            next();
+    }
+
+    function _onClose() {
+        _beforeClose(onClose);
+    }
+
+    function _logError(e) {
+        window.console.error(errorPrefix, e);
+    }
+
+    function _onError(e,x) {
+        console.error(x);
+        _logError(e);
+        _beforeClose(function () {
+            if (onError)
+                onError(e);
+        });
+    }
+
+    try {
+        options = {};
+
+        if (!options.title)
+            options.title = url.split('/').pop(); // strip file name from url
+        if(!url)
+            onError();
+        else {
+            _beforeShow(function () {
+                exec(
+                    function (result) {
+                        var status = result ? result.status : null;
+
+                        if (status == 1) {
+                            _onShow();
+                        }
+                        else if (status == 0) {
+                            _onClose();
+                        }
+                        else {
+                            var errorMsg =
+                                "unknown state '" + status
+                                + "'";
+                            window.console.error(
+                                errorPrefix + errorMsg);
+                        }
+                    },
+                    _onError,
+                    CDV_HANDLE,
+                    CDV_HANDLE_ACTIONS.VIEW_DOCUMENT,
+                    [
+                        {
+                            url: url,
+                            contentType: contentType,
+                            options: options
+                        }
+                    ]
+                );
+            });
+        }
+
+    }
+    catch (e) {
+        _onError(e);
+    }
+}
